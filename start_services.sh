15af5c9e801223f1fc5c69f2c9e7a9aeccf95c38
Chai77 <chaitanyaeranki@y7mail.com>
2024-04-26 16:50:27 -0500

    Made start_services output to log files

--- start_services.sh
+++ start_services.sh
@@ -5,14 +5,14 @@ export ROBOFLEET_SERVER_PORT=8085
 SAVED_PATH=$PWD
 
 cd $SAVED_PATH/robofleet_client
-ROS_NAMESPACE="leva" make run &
+ROS_NAMESPACE="leva" make run > $HOME_DIR/robofleet_client.log &
 echo "Started client"
 
 cd $SAVED_PATH/robofleet_server
-yarn start &
+yarn start > $HOME_DIR/robofleet_server.log &
 echo "Started server"
 
 cd $SAVED_PATH/robofleet_webviz
 export NODE_OPTIONS=--openssl-legacy-provider
-yarn start
+yarn start > $HOME_DIR/robofleet_webviz.log &
 echo "Started webviz"
